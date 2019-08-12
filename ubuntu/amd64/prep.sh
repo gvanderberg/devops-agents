966e95a7c806e17c6ac7417d39a955f3ccdee9b2
Gareth van der Berg <gareth.shaun@gmail.com>
2019-08-12 11:11:47 +0200

    Update prep.sh

--- ubuntu/amd64/prep.sh
+++ ubuntu/amd64/prep.sh
@@ -50,7 +50,7 @@ echo 4\) Install Azure CLI
 echo
 
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/azure-cli.list
-curl -L https://packages.microsoft.com/keys/microsoft.asc | apt-key add - \
+curl -L https://packages.microsoft.com/keys/microsoft.asc | apt-key add - 
 apt-get update
 apt-get install -y --no-install-recommends apt-transport-https azure-cli
 rm -rf /var/lib/apt/lists/* 
