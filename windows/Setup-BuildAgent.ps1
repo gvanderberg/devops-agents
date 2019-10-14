057f0e3fd0dc986abed259c68b33edfb5b2d3a97
Gareth van der Berg <gareth.shaun@gmail.com>
2019-10-14 16:00:13 +0200

    Update Setup-BuildAgent.ps1

--- windows/Setup-BuildAgent.ps1
+++ windows/Setup-BuildAgent.ps1
@@ -1,2 +1,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install -y jdk8
+choco install -y nodejs-lts
