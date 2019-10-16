5f1405591d8110ddff5c885077ab1f6bbab8184e
Gareth van der Berg <gareth.shaun@gmail.com>
2019-10-16 15:08:37 +0200

    Update and rename windows/server-2016/Setup-BuildAgent.ps1 to windows/server/Setup-BuildAgent.ps1

--- windows/server-2016/Setup-BuildAgent.ps1
+++
@@ -1,7 +0,0 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-choco install -y jdk8
-choco install -y nodejs-lts
-choco install -y terraform
-choco install -y yarn
