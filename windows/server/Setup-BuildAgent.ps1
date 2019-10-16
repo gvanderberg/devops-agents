56e6933cf284b42744673261645d374e33d3b1a8
Gareth van der Berg <gareth.shaun@gmail.com>
2019-10-16 15:13:20 +0200

    Update Setup-BuildAgent.ps1

--- windows/server/Setup-BuildAgent.ps1
+++ windows/server/Setup-BuildAgent.ps1
@@ -2,10 +2,12 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco install -y azure-cli
+choco install -y dotnetcore-sdk
 choco install -y jdk8
 choco install -y kubernetes-cli
 choco install -y kubernetes-helm
 choco install -y nodejs-lts
+choco install -y powershell-core
 choco install -y terraform
 choco install -y yarn
 choco install -y visualstudio2019enterprise --package-parameters "--add Microsoft.VisualStudio.Workload.CoreEditor --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetCoreTools --add Microsoft.VisualStudio.Workload.NetCrossPlat --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Node --passive --locale en-US"
