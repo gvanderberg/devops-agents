6533398d596b971d41da5f9cbef1bcbd7d493cd8
Gareth van der Berg <gareth.shaun@gmail.com>
2019-10-16 15:11:37 +0200

    Update Setup-BuildAgent.ps1

--- windows/desktop/Setup-BuildAgent.ps1
+++ windows/desktop/Setup-BuildAgent.ps1
@@ -7,6 +7,7 @@ choco install -y jdk8
 choco install -y kubernetes-cli
 choco install -y kubernetes-helm
 choco install -y nodejs-lts
+choco install -y powershell-core
 choco install -y terraform
 choco install -y yarn
 choco install -y visualstudio2019enterprise --package-parameters "--add Microsoft.VisualStudio.Workload.CoreEditor --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetCoreTools --add Microsoft.VisualStudio.Workload.NetCrossPlat --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Node --passive --locale en-US"
