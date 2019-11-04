0f2e371ff3eb02a7c565e546a03c5318b8a05f20
Gareth van der Berg <gareth.shaun@gmail.com>
2019-11-04 18:17:52 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,47 +1,38 @@
 #!/bin/bash
 
-: ${USE_SUDO:="true"}
-
 # initArch discovers the architecture for this system.
 initArch() {
-  ARCH=$(uname -m)
-  case $ARCH in
-    armv5*) ARCH="armv5";;
-    armv6*) ARCH="armv6";;
-    armv7*) ARCH="arm";;
-    aarch64) ARCH="arm64";;
-    x86) ARCH="386";;
-    x86_64) ARCH="amd64";;
-    i686) ARCH="386";;
-    i386) ARCH="386";;
-  esac
+    ARCH=$(uname -m)
+    case $ARCH in
+    armv5*) ARCH="armv5" ;;
+    armv6*) ARCH="armv6" ;;
+    armv7*) ARCH="arm" ;;
+    aarch64) ARCH="arm64" ;;
+    x86) ARCH="386" ;;
+    x86_64) ARCH="amd64" ;;
+    i686) ARCH="386" ;;
+    i386) ARCH="386" ;;
+    esac
+}
+
+initCodename() {
+    CODENAME=$(lsb_release -cs)
 }
 
 # initOS discovers the operating system for this system.
 initOS() {
-  OS=$(echo `uname`|tr '[:upper:]' '[:lower:]')
+    OS=$(echo $(uname) | tr '[:upper:]' '[:lower:]')
 
-  case "$OS" in
+    case "$OS" in
     # Minimalist GNU for Windows
-    mingw*) OS='windows';;
-  esac
-}
-
-# runs the given command as root (detects if we are root already)
-runAsRoot() {
-  local CMD="$*"
-
-  if [ $EUID -ne 0 -a $USE_SUDO = "true" ]; then
-    CMD="sudo $CMD"
-  fi
-
-  $CMD
+    mingw*) OS='windows' ;;
+    esac
 }
 
 initArch
+initCodename
 initOS
-runAsRoot
 
 echo $ARCH
+echo $CODENAME
 echo $OS
-echo $CMD
