adbb902b87ad95aa1be9af3d624eb764a863bb08
Gareth van der Berg <gareth.shaun@gmail.com>
2019-12-20 13:11:52 +0200

    add agent config

---
+++ windows/Config-BuildAgent.ps1
@@ -0,0 +1,36 @@
+param (
+    [string]$url,
+    [string]$token,
+    [string]$pool,
+    [string]$agent,
+    [string]$path
+)
+
+$destinationPath = Join-Path -Path $path -ChildPath $agent
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$wr = Invoke-WebRequest https://api.github.com/repos/Microsoft/azure-pipelines-agent/releases/latest
+$tag = ($wr | ConvertFrom-Json)[0].tag_name
+$tag = $tag.Substring(1)
+
+$agentUrl = "https://vstsagentpackage.azureedge.net/agent/$tag/vsts-agent-win-x64-$tag.zip"
+
+Write-Host "Drive: $drive `nURL: $url `nToken: $token `nPool: $pool `nAgent: $agent `nAgentVersion: $tag `nAgentUrl: $agentUrl" 
+
+if (Test-Path $destinationPath) {
+    Write-Host "Deleteing folder " $destinationPath
+    Remove-Item -Path $destinationPath -Force -Confirm:$false -Recurse
+}
+
+New-Item -ItemType Directory -Force -Path $destinationPath
+
+Write-Host $destinationPath
+# Set-Location $destinationPath
+
+Invoke-WebRequest $agentUrl -Out agent.zip
+Expand-Archive -Path agent.zip -DestinationPath $destinationPath
+
+& $destinationPath\config.cmd --unattended --replace --url $url --auth pat --token $token --pool $pool --agent $agent --acceptTeeEula --runAsService
+
+exit 0
